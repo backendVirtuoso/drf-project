ba564b54fa15c915577c69be1823a1d403399cdc
backendVirtuoso <129765568+backendVirtuoso@users.noreply.github.com>
2026-01-05 17:01:46 +0900

    feat. 동기 vs 비동기

---
+++ sec 2. 파이썬 코루틴과 비동기 함수/2-2. async.py
@@ -0,0 +1,21 @@
+import asyncio
+import time
+
+async def delivery(name, mealtime):
+    print(f"{name}에게 배달 완료!")
+    await asyncio.sleep(mealtime)
+    print(f"{name}이(가) 식사 완료, {mealtime}시간 소요")
+    print(f"{name} 그릇 수거 완료")
+
+async def main():
+    await asyncio.gather(
+        delivery("A", 5),
+        delivery("B", 3),
+        delivery("C", 4)
+    )
+    
+if __name__ == "__main__":
+    start = time.time()
+    asyncio.run(main()) 
+    end = time.time()
+    print(f"총 소요 시간: {end - start}")
